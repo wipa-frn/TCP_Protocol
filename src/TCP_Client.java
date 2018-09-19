5fda45bf4c6a17b47b9b89b08eb0ff7064818b43
thikamporn <thikamporn.s@ku.th>
2018-09-19 07:10:43 +0700

    edit some bug

--- src/TCP_Client.java
+++ src/TCP_Client.java
@@ -1,30 +1,42 @@
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 import java.io.*;
 import java.net.*;
 public class TCP_Client {
     public static void main(String[] args) throws Exception
     {
-
         Socket connectS = new Socket("localhost",9999);
         System.out.println("Send something to server.");
-        DataOutputStream dout=new DataOutputStream(connectS.getOutputStream());
+        DataOutputStream dOut = new DataOutputStream(connectS.getOutputStream());
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         DataInputStream dIn=new DataInputStream(connectS.getInputStream());
 
         while(true) {
             String sendMassage = br.readLine();
-            dout.writeUTF(sendMassage);
-            if(sendMassage.equals("GOODBYE")) {
-                String massage = dIn.readUTF();
-                System.out.println(massage);
-                break;
+            dOut.writeUTF(sendMassage);
 
-            }
             //read from server
             String massage = dIn.readUTF();
-            if (!massage.equals(""))
+            if (!massage.equals("")) System.out.println("Server: " + massage);
+            else if (massage.matches( connectS.getLocalAddress().toString())) {
+                dOut.writeUTF("EXIT");
+                dOut.flush();
+                break;
+            }
+
+            /*if(sendMassage.equals("GOODBYE")) {
+                massage = dIn.readUTF();
                 System.out.println(massage);
 
+            }
+            else if (sendMassage.equals("EXIT")){
+                dOut.flush();
+                break;
+            }*/
+
+
         }
-        connectS.close();}
+        connectS.close();
+    }
 
 }
--- src/TCP_Server.java
+++ src/TCP_Server.java
@@ -2,40 +2,39 @@ import java.io.*;
 import java.net.*;
 public class TCP_Server {
 
-    private static User user1 = new User("FERN","123456",new DebitCard("1234567890123456","121"));
+    private static User user1 = new User("FERN","123456", new DebitCard("1234567890123456","121"));
 
     public static void main(String[] args) throws Exception {
 
 
         ServerSocket serverSocket=new ServerSocket(9999);
 
-        System.out.println("server online..");
+        System.out.println("Top UP Easy Server online..");
         Socket client = serverSocket.accept();
-        String addr = client.getLocalAddress()+" is connected";
-        System.out.println(addr);
+        String address = "Client form " + client.getLocalAddress() + " is connected";
+        System.out.println(address);
 
 
         //connection with client
-        DataInputStream dIn=new DataInputStream(client.getInputStream());
-        DataOutputStream dout=new DataOutputStream(client.getOutputStream());
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        DataInputStream dIn = new DataInputStream(client.getInputStream());
+        DataOutputStream dOut = new DataOutputStream(client.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String sendM;
         int statusLogin = 0;
         String phoneNum = null;
-        String sim = null;
+        String networkSim = null;
         String money = null;
 
         while(true) {
-
             String massage = dIn.readUTF();
-            System.out.println("client: "+massage);
+            System.out.println("Client: " + massage);
 
             if(statusLogin == 0) { //non login.
 
                 if (massage.equals("HI")) {
-                    sendM = "HELLO Please login to Top-Up easy.";
-                    dout.writeUTF(sendM);
+                    sendM = "HELLO " + client.getLocalAddress() + " , please login to Top-Up easy.";
+
 
                 } else if (massage.matches("LOG:USER=(.*)")) {
                     String str = massage;
@@ -46,58 +45,56 @@ public class TCP_Server {
                     else
                         sendM = "User name is not correct";
 
-                    dout.writeUTF(sendM);
 
                 } else if (massage.matches("LOG:PWD=(.*)")) {
                     String str = massage.replaceFirst("LOG:PWD=", "");
-                    System.out.println(str);
 
                     if (str.equals(user1.getPassword()))
                         sendM = "Login success! Welcome " + user1.getUsername();
                     else
                         sendM = "Password is not correct";
 
-                    dout.writeUTF(sendM);
                     statusLogin = 1;
 
                 }
                 else{
-                    dout.writeUTF("");
+                    sendM = "Your syntax is error";
                 }
+                dOut.writeUTF(sendM);
             }
-            else if(statusLogin == 1){ //login success
+            else if(statusLogin == 1){ //when login success
 
                 if ( massage.matches("TUP=(.*)")) {
                     String str = massage.replaceFirst("TUP=", "");
 
                     if (str.equals("AIS")||str.equals("DTAC")||str.equals("TRUE")){
                         sendM = "You choose "+ str + ". Are you sure? [Y/N]";
-                        dout.writeUTF(sendM);
+                        dOut.writeUTF(sendM);
 
                         String msg = dIn.readUTF();
-                        System.out.println("client: "+msg);
+                        System.out.println("client: "+ msg);
 
                         if(msg.equals("Y")){
                             sendM = "PLease enter your phone number";
-                            dout.writeUTF(sendM);
-                            sim = str;
+                            dOut.writeUTF(sendM);
+                            networkSim = str;
                         }else {
-                            sim = null;
-                            dout.writeUTF("");
+                            networkSim = null;
+                            dOut.writeUTF("");
                         }
                     }
                     else {
                         sendM = "Sim is not correct";
-                        dout.writeUTF(sendM);
+                        dOut.writeUTF(sendM);
                     }
                 }
-                else if ((!sim.isEmpty()) && massage.matches("NUM=(.*)")){
+                else if ((!networkSim.isEmpty()) && massage.matches("NUM=(.*)")){
 
                     String str = massage.replaceFirst("NUM=", "");
 
                     if(str.matches("([0-9]*)") && (str.length() == 10) ){
                         sendM = "Phone number is "+str+". Are you sure? [Y/N]";
-                        dout.writeUTF(sendM);
+                        dOut.writeUTF(sendM);
 
                         String msg = dIn.readUTF();
                         System.out.println("client: "+msg);
@@ -105,16 +102,16 @@ public class TCP_Server {
                         if(msg.equals("Y")){
                             phoneNum = str ;
                             sendM = "Please enter money";
-                            dout.writeUTF(sendM);
+                            dOut.writeUTF(sendM);
 
                         }else if (msg.equals("N")) {
                             phoneNum = null;
-                            dout.writeUTF("");
+                            dOut.writeUTF("");//
                         }
                     }
                     else {
                         sendM = "Phone number is not correct.";
-                        dout.writeUTF(sendM);
+                        dOut.writeUTF(sendM);
                     }
                 }
 
@@ -123,11 +120,11 @@ public class TCP_Server {
 
                     if(str.equals("100") || str.equals("200") || str.equals("500") || str.equals("1000")){
                         sendM = "PLease enter your debit card number (ending with _ ) and then followed by CVV number";
-                        dout.writeUTF(sendM);
+                        dOut.writeUTF(sendM);
                         money = str;
                     }else {
-                        sendM = "Money out of rank.";
-                        dout.writeUTF(sendM);
+                        sendM = "Money out of range.";
+                        dOut.writeUTF(sendM);
                     }
 
                 }else if(!money.isEmpty() && massage.matches("BNK=(.*)")){
@@ -135,38 +132,40 @@ public class TCP_Server {
 
                     if(str.equals(user1.getDebitCard().getNumberDebit()+"_"+user1.getDebitCard().getNumberCVV())){
                         sendM = "Are you confirm TOP-UP? [Y/N]";
-                        dout.writeUTF(sendM);
+                        dOut.writeUTF(sendM);
 
                         String msg = dIn.readUTF();
                         System.out.println("client: "+msg);
 
                         if(msg.equals("Y")){
                             sendM = "TOP-UP is success GOODBYE.";
-                            dout.writeUTF(sendM);
-                            sim = null;
+                            dOut.writeUTF(sendM);
+                            networkSim = null;
                             phoneNum = null;
                             money = null;
                             statusLogin = 0;
                         }
                     }
                     else{
-                        sendM = "Your debitcard number or CVV number is not correct";
-                        dout.writeUTF(sendM);
+                        sendM = "Your debit card number or CVV number is not correct";
+                        dOut.writeUTF(sendM);
                     }
                 }
                 else{
-                    dout.writeUTF("");
+                    dOut.writeUTF("Server: Your syntax is error");
                 }
 
             //when client say goodbye
-            }else if (massage.equals("GOODBYE")) {
-                dout.writeUTF("GOODBYE");
-                System.out.println("Server closed.");
+            }else if (massage.equals("GOODBYE") && statusLogin == 0) {
+                dOut.writeUTF(client.getLocalAddress().toString());
+            }
+            else if (massage.equals("EXIT")) {
                 break;
             }
             else{
-                dout.writeUTF("");
+                dOut.writeUTF("Server: Your syntax is error");
             }
+
         }
         client.close();
     }
