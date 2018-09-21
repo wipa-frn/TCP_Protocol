e30f78ba8b39186f7bfab8ebbdb9478f9895d5df
thikamporn <thikamporn.s@ku.th>
2018-09-21 17:44:12 +0700

    modify client

--- src/TCP_Client.java
+++ src/TCP_Client.java
@@ -6,19 +6,17 @@ public class TCP_Client {
         Socket connectS = new Socket("localhost",9999);
         System.out.println("Send something to server.");
         DataOutputStream dOut = new DataOutputStream(connectS.getOutputStream());
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        DataInputStream dIn=new DataInputStream(connectS.getInputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        DataInputStream dIn  =new DataInputStream(connectS.getInputStream());
         try {
             while (true) {
-                String sendMassage = br.readLine();
-                dOut.writeUTF(sendMassage);
+                String sendMessage = br.readLine();
+                dOut.writeUTF(sendMessage);
 
                 //read from server
                 String message = dIn.readUTF();
-                if (!message.equals("")) System.out.println("Server: " + message);
-                else if (message.matches(connectS.getLocalAddress().toString())) {
-                    dOut.writeUTF("EXIT");
-                    dOut.flush();
+                if (!message.equals("")) System.out.println("S: " + message);
+                if (message.matches("101 DISCONNECTED" + " > " + connectS.getLocalAddress().toString() + " is logout")) {
                     break;
                 }
             }
@@ -29,33 +27,6 @@ public class TCP_Client {
         finally {
             connectS.close();
         }
-
-        /*while(true) {
-            *//*String sendMassage = br.readLine();
-            dOut.writeUTF(sendMassage);
-
-            //read from server
-            String massage = dIn.readUTF();
-            if (!massage.equals("")) System.out.println("Server: " + massage);
-            else if (massage.matches( connectS.getLocalAddress().toString())) {
-                dOut.writeUTF("EXIT");
-                dOut.flush();
-                break;
-            }*//*
-
-            *//*if(sendMassage.equals("GOODBYE")) {
-                massage = dIn.readUTF();
-                System.out.println(massage);
-
-            }
-            else if (sendMassage.equals("EXIT")){
-                dOut.flush();
-                break;
-            }*//*
-
-
-        }*/
-
     }
 
 }
